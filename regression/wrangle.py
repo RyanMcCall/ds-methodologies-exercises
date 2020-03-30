af50c3b5ddd46cd05e4b4cb71ea0752c8fa2ecdb
Ryan McCall <ryan.e.mccall@gmail.com>
2020-03-30 10:50:47 -0500

    Start wrangling

---
+++ regression/wrangle.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+from env import get_url
+
+def wrangle_telco():
+    url = get_url('telco_churn')
+
+    query = '''
+    SELECT customer_id, monthly_charges, tenure, total_charges
+    FROM customers
+    JOIN contract_types USING (contract_type_id)
+    WHERE contract_type = 'Two year'
+    '''
+
+    df = pd.read_sql(query, url)
+
+    df.replace(' ', np.nan, inplace=True)
+
+    df = df.dropna()
+
+    df.total_charges = df.total_charges.astype('float')
+
+    return df
