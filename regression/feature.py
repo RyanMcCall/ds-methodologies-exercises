e95eee5a8eb1e53a985c0a2dfc80de8a019fc955
Ryan McCall <ryan.e.mccall@gmail.com>
2020-04-04 18:28:15 -0500

    Add feature engineering functions

---
+++ regression/feature.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+from sklearn.feature_selection import SelectKBest, f_regression, RFE
+from sklearn.linear_model import LinearRegression
+
+def select_kbest(X, y, k):
+    '''
+    X: pd.DataFrame; Scaled features
+    y: pd.DataFrame; Scaled target
+    k: int; number of features to return
+    
+    Returns a list of the column names that are the k best features
+    '''
+    f_selector = SelectKBest(f_regression, k=k)
+    f_selector.fit(X, y)
+
+    f_support = f_selector.get_support()
+    f_feature = X.loc[:,f_support].columns.tolist()
+    return f_feature
+
+def rfe(X, y, k):
+    '''
+    X: pd.DataFrame; Scaled features
+    y: pd.DataFrame; Scaled target
+    k: int; number of features to return
+    
+    Returns a list of the column names that are the k best features
+    '''
+    lm = LinearRegression()
+    rfe = RFE(lm, k)
+    rfe.fit(X, y)
+
+    rfe_features = X.loc[:,rfe.support_].columns.tolist()
+    return rfe_features
