04ebd9fadf0d060d868e125c34d344bf4242058d
Ryan McCall <ryan.e.mccall@gmail.com>
2020-04-13 17:19:37 -0500

    Complete exercises

---
+++ classification/acquire.py
@@ -0,0 +1,23 @@
+import env
+import pandas as pd
+
+def get_titanic_data():
+    url = env.get_url('titanic_db')
+
+    query = '''
+    SELECT * 
+    FROM passengers
+    '''
+
+    return pd.read_sql(query, url, index_col='passenger_id')
+
+def get_iris_data():
+    url = env.get_url('iris_db')
+
+    query = '''
+    SELECT m.*, s.species_name
+    FROM measurements m
+    JOIN species s ON s.species_id = m.species_id;
+    '''
+
+    return pd.read_sql(query, url, index_col='measurement_id')
